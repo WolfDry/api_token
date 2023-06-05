590ea2a523baed23ebdf51ae58d1bffd25164d8c
Benjamin <benjamindeltour22@gmail.com>
2023-06-05 10:36:24 +0200

    bug need to be fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,33 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Roles } from 'src/security/roles.decorator';
+import { UserRole } from './entities/user.entity';
+import { RolesGuard } from 'src/security/roles.guard';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
+  @Roles(UserRole.ADMIN)
+  @UseGuards(RolesGuard)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @Roles(UserRole.MEMBER)
+  @UseGuards(RolesGuard)
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @Roles(UserRole.ADMIN, UserRole.MEMBER)
+  @UseGuards(RolesGuard)
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
+  @Roles(UserRole.ADMIN)
+  @UseGuards(RolesGuard)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @Roles(UserRole.ADMIN)
+  @UseGuards(RolesGuard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
